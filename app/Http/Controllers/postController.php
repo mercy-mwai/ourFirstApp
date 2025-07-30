f89841692e3ec97ef7bbd8b2cf0e91968ba60574
mercy-mwai <mercyneemam@gmail.com>
2025-07-30 11:38:07 +0300

    updates

--- app/Http/Controllers/postController.php
+++ app/Http/Controllers/postController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class postController extends Controller
 {
     public function createPost(){
-        
+        $incomingFields=$request->validate([
+            'title'=>'required',
+            'password'=>'required',
+        ]);
+
+        $incomingFields['title']=strip_tags($incomingFields['title']);
+        $incomingFields['body']=strip_tags($incomingFields['body']);
+        $incomingFields['user_id']=auth()->id();
+
+        Post::create($incomingFields);
     }
 }
